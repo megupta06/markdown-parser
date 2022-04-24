4ebc0ee6ce205339fee1c3787c7f6239b8a6f63f
megupta06 <megupta@ucsd.edu>
2022-04-24 15:08:28 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,9 +12,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
 
-        if (!markdown.substring(currentIndex).contains("(")){
-            return toReturn;
-        }
+       
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
