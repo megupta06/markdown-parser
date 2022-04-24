166e3533122ab8b8b89016ea8b6f8c1e81a4395f
megupta06 <megupta@ucsd.edu>
2022-04-24 14:02:24 -0700

    final changes1

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,10 +17,11 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (openBracket==0 || !(markdown.charAt(openBracket-1)=='!')){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (openBracket==0 || markdown.charAt(openBracket-1)=='!'){
+                currentIndex = closeParen+1;
+                continue;
             }
-            
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
 
